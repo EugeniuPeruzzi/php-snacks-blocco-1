43d189e352993744e0d552b93089d71b964db7e0
EugeniuPeruzzi <jaka1997@live.it>
2023-07-03 16:12:02 +0200

    [snack 3] fixed

---
+++ snack-3/index.php
@@ -0,0 +1,82 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: 
+    DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+    Stampare ogni data con i relativi post. -->
+
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Snack 3</title>
+</head>
+<body>
+    <div class="m-5">
+        <?php foreach($posts as $index => $post) { ?>
+            <h5><?php echo $index ?></h5>
+            <ul>
+                <?php foreach ($post as $content){ ?>
+                    <li>
+                        <h6><?php echo $content['title'] ?></h6>
+                        <ul>
+                            <li>
+                                <h6><?php echo $content['author'] ?></h6>
+                            </li>
+                            <li>
+                                <h6><?php echo $content['text'] ?></h6>
+                            </li>
+                            <hr>
+                        </ul>
+                    </li>
+                <?php }?>
+            </ul>
+        <?php } ?>
+    </div>
+</body>
+</html>
